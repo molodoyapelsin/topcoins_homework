43feebbc0fc4708cfce4714efaa57a53fd115568
molodoyapelsin <jacquiw732@gmail.com>
2025-09-15 17:05:55 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,10 @@
+metres = int(input())
+# перевод в сантиметры
+print (f"{metres} metres equals {metres * 100} sentimetres.")
+# перевод в дециметры
+print (f"Also, {metres} metres equals {metres * 10} decimetres.")
+# перевод в миллиметры
+print (f"Also, {metres} metres equals {metres * 1000} millimetres.")
+# перевод в мили
+print (f"Also, {metres} metres equals {metres * 0.0006213712} milles.")
+# damn son
