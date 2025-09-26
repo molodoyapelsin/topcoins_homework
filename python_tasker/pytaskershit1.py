8c96b3e10346146b1ae2b65d6c56bc02cbb2b954
molodoyapelsin <jacquiw732@gmail.com>
2025-09-26 13:12:15 +0300

    Add files via upload
    №1 - Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.

---
+++ python_tasker/pytaskershit1.py
@@ -0,0 +1,6 @@
+print("Введите ваше число.")
+thenumbah = int(input())
+if thenumbah < 0:
+    print(f"Число '{thenumbah}' - отрицательное.")
+else:
+    print(f"Число '{thenumbah}' не отрицательное.")
