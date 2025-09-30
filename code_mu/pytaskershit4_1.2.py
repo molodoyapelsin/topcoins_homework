80fc124676204ed338b7102cb2dc2b32c011727b
molodoyapelsin <jacquiw732@gmail.com>
2025-09-30 13:05:44 +0300

    Add files via upload

---
+++ code_mu/pytaskershit4_1.2.py
@@ -0,0 +1,5 @@
+# Дано число. Выведите количество цифр в этом числе.
+print("Введите число, количество цифр в котором вы хотите узнать.")
+thefuckingnumber = int(input())
+number = str(thefuckingnumber)
+print(f"В числе '{thefuckingnumber}' - {len(number)} цифр.")
