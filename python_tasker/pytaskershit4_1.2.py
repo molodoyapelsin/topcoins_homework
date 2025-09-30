eb6477a8c68101295e51e17a4cf2a3d05a190675
molodoyapelsin <jacquiw732@gmail.com>
2025-09-30 13:05:19 +0300

    Delete python_tasker directory

--- python_tasker/pytaskershit4_1.2.py
+++
@@ -1,5 +0,0 @@
-# Дано число. Выведите количество цифр в этом числе.
-print("Введите число, количество цифр в котором вы хотите узнать.")
-thefuckingnumber = int(input())
-number = str(thefuckingnumber)
-print(f"В числе '{thefuckingnumber}' - {len(number)} цифр.")
